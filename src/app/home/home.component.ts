955b36d3a6d3d1f38408b39b92b53c84767646bf
maha.hmila@gmail.com <maha.hmila@gmail.com>
2018-10-10 11:49:13 +0200

    design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup } from '@angular/forms';
 import { ApiTodoService } from '../api-todo.service';
 import * as jwt_decode from 'jwt-decode';
-import { Token } from '@angular/compiler';
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
-
-
 
 @Component({
   selector: 'app-home',
@@ -18,9 +15,7 @@ export class HomeComponent implements OnInit {
   index = 1;
   public todos: Array<object> = [];
   homeForm: FormGroup;
-
   id = '';
-
 
   constructor(private router: Router, private apitodoservice: ApiTodoService, private cookieService: CookieService) {
     this.homeForm = new FormGroup({
@@ -32,22 +27,24 @@ export class HomeComponent implements OnInit {
     if (!test) {
       this.router.navigate(['login']);
     }
-
     const decoded = jwt_decode(test);
     this.id = decoded.data._id;
     this.getTodos();
-
-
     // console.log(test);
   }
 
-
   addTodo() {
-    console.log(this.homeForm.value.todo);
+    const today = new Date();
+    const dd = today.getDate();
+    const mm = today.getMonth() + 1;
+    const yyyy = today.getFullYear();
+    const tod = mm + '-' + dd + '-' + yyyy;
+
+    // console.log(this.homeForm.value.todo);
     // const decUser = decrypt(userToken);
     const todo = {
       content: this.homeForm.value.todo,
-      date: Date().toString(),
+      date: tod,
       done: 'false'
     };
     this.apitodoservice.addTodoApi(this.id, todo).subscribe(res => {
@@ -59,7 +56,6 @@ export class HomeComponent implements OnInit {
     console.log(this.id);
     this.apitodoservice.getTodosApi(this.id).subscribe((data: Array<object>) => {
       this.todos = data.todo;
-
       // console.log(this.todos);
     });
   }
@@ -75,14 +71,16 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['login']);
   }
 
-  checkValue(event: any) {
-    console.log(event);
-  }
-
   FieldsChange(index, values: any) {
-    console.log('done', values.currentTarget.checked, 'index', index);
+    if (values.currentTarget.checked === true) {
+       document.getElementById(index).style.cssText = 'color: #CCC; text-decoration: line-through;';
+    } else {
+      document.getElementById(index).style.cssText = '';
+    }
+
+    // console.log('done', values.currentTarget.checked, 'index', index);
     this.todos[index].done = values.currentTarget.checked;
-    console.log(this.todos[index]);
+    // console.log(this.todos[index]);
     return this.apitodoservice.updateDone(this.id, index, this.todos[index]).subscribe(res => {
      });
   }
